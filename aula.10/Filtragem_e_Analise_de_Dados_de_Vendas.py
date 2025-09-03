c69dadc29543af3d6c422c040d7bc107cb650690
Grediness <168847376+Grediness@users.noreply.github.com>
2025-09-03 10:19:23 -0300

    Filtragem_e_Analise_de_Dados_de_Vendas.

---
+++ aula.10/Filtragem_e_Analise_de_Dados_de_Vendas.py
@@ -0,0 +1,24 @@
+class Dados:
+    def __init__(self):
+        self.vendas=[('Teclado',50,2),('Mouse',25.50,4),('Monitor',300,1),('Fone',45,1),('Webcam',75.20,2)]
+    def verificar(self):
+        self.nova_lista=list()
+        produtos_unicos=set()
+        try:
+            for nome,preco,qtn in self.vendas:
+            
+                self.total=preco*qtn
+                if self.total>=100:
+                    
+                        self.nova_lista.append((nome,preco,qtn))
+                produtos_unicos.add(nome)
+            print(self.nova_lista)
+            
+            
+            print(produtos_unicos)
+        except ValueError:
+            print('O valor passdo está incorreto')
+    
+
+a=Dados()
+a.verificar()
